6f786acfe339c5a497e9ac191e44594c548e2e9d
YummyBerry <bertylliu@gmail.com>
2023-02-11 23:52:02 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# web
+## This is just a blog for myself
+此網頁的圖片和音樂的`版權皆不屬於我`
+
+The `copyright` of the images or musics in the website `is not belong to me`
---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+var blog = document.getElementById("Blog");
+var support = document.getElementById("support");
+var about = document.getElementById("about");
+var artical = document.getElementById("artical");
+var zrush = document.getElementById("zrushSick");
+var count = 0;
+
+blog.addEventListener("mouseover", function(){
+    this.innerText = "ㄆㄊ的部落格"
+})
+blog.addEventListener("mouseout",function(){
+    this.innerText = "Berry Blog"
+})
+
+// support.addEventListener("mouseover", function(){
+//     this.innerText = "支持億下"
+// })
+// support.addEventListener("mouseout",function(){
+//     this.innerText = "支持一下"
+// })
+
+about.addEventListener("mouseover", function(){
+    this.innerText = "還沒做"
+})
+about.addEventListener("mouseout",function(){
+    this.innerText = "作品集"
+})
+
+artical.addEventListener("mouseover", function(){
+    this.innerText = "沒有"
+})
+artical.addEventListener("mouseout",function(){
+    this.innerText = "文章"
+})
+//-----------------click----------------------//
+function toggle_info() {
+    document.getElementById("profile-info").classList.toggle("invisible");
+}
+function load() {
+    document.getElementById("loading").classList.toggle("invisible")
+}
+
