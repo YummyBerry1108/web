94575678978d12a3a4f2fca24d2414ead059f5fb
YummyBerry <bertylliu@gmail.com>
2023-04-06 23:33:09 +0800

    5

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 var blog = document.getElementById("Blog");
-var support = document.getElementById("support");
-var about = document.getElementById("about");
-var artical = document.getElementById("artical");
+// var support = document.getElementById("support");
+// var about = document.getElementById("about");
+// var artical = document.getElementById("artical");
 var zrush = document.getElementById("zrushSick");
 var count = 0;
 
@@ -19,19 +19,19 @@ blog.addEventListener("mouseout",function(){
 //     this.innerText = "支持一下"
 // })
 
-about.addEventListener("mouseover", function(){
-    this.innerText = "還沒做"
-})
-about.addEventListener("mouseout",function(){
-    this.innerText = "作品集"
-})
+// about.addEventListener("mouseover", function(){
+//     this.innerText = "還沒做"
+// })
+// about.addEventListener("mouseout",function(){
+//     this.innerText = "作品集"
+// })
 
-artical.addEventListener("mouseover", function(){
-    this.innerText = "沒有"
-})
-artical.addEventListener("mouseout",function(){
-    this.innerText = "文章"
-})
+// artical.addEventListener("mouseover", function(){
+//     this.innerText = "沒有"
+// })
+// artical.addEventListener("mouseout",function(){
+//     this.innerText = "文章"
+// })
 //-----------------click----------------------//
 function toggle_info() {
     document.getElementById("profile-info").classList.toggle("invisible");
